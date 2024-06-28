d35d193e193f23fad32226889444f68e1abebc66
nathanael-adi-t <nathanael@kinnarastudio.com>
2024-06-28 11:28:40 +0700

    document list response update

--- src/main/java/com/kinnarastudio/commons/mekarisign/model/GetDocumentListsResponse.java
+++ src/main/java/com/kinnarastudio/commons/mekarisign/model/GetDocumentListsResponse.java
@@ -1,5 +1,22 @@
 package com.kinnarastudio.commons.mekarisign.model;
 
 public class GetDocumentListsResponse {
-    
+    private final SignResponseAttributes[] signResponseAttributes;
+    private final int currentPage;
+    private final int previous;
+    private final int next;
+    private final int perPage;
+    private final int totalPage;
+    private final int documentCount;
+
+    public GetDocumentListsResponse (SignResponseAttributes[] signResponseAttributes, int currentPage, int previous, int next, int perPage, int totalPage, int documentCount)
+    {
+        this.signResponseAttributes = signResponseAttributes;
+        this.currentPage = currentPage;
+        this.previous = previous;
+        this.next = next;
+        this.perPage = perPage;
+        this.totalPage = totalPage;
+        this.documentCount = documentCount;
+    }
 }
