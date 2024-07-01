4ab87e0efe39f4797a7e27260d488455fbb7e781
hendrikavaleska <hendrika.andhela@kinnarastudio.com>
2024-07-01 10:04:51 +0700

    parsing date update

--- src/main/java/com/kinnarastudio/commons/mekarisign/model/SignResponseAttributes.java
+++ src/main/java/com/kinnarastudio/commons/mekarisign/model/SignResponseAttributes.java
@@ -5,10 +5,9 @@ import com.kinnarastudio.commons.jsonstream.JSONStream;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.text.ParseException;
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
-
-import java.text.ParseException;
 import java.util.Date;
 
 public class SignResponseAttributes {
@@ -19,10 +18,10 @@ public class SignResponseAttributes {
     private final String stampingStatus;
     private final String typeOfMaterai;
     private final ResponseSigner[] responseSigner;
-    private final Date createdAt = new Date();
-    private final Date updatedAt = new Date();
+    private final Date createdAt;
+    private final Date updatedAt;
 
-    public SignResponseAttributes(String filename, AttributeCategory category, String docUrl, SigningStatus signingStatus, String stampingStatus, String typeOfMaterai, ResponseSigner[] responseSigner, Date createdAt, Date updatedAt) {
+    public SignResponseAttributes(String filename, AttributeCategory category, String docUrl, SigningStatus signingStatus, String stampingStatus, String typeOfMaterai, ResponseSigner[] responseSigner, Date createdAt, Date updatedAt, Date createdAt1, Date updatedAt1) {
         this.filename = filename;
         this.category = category;
         this.docUrl = docUrl;
@@ -30,19 +29,19 @@ public class SignResponseAttributes {
         this.stampingStatus = stampingStatus;
         this.typeOfMaterai = typeOfMaterai;
         this.responseSigner = responseSigner;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
     }
 
-
-    public SignResponseAttributes(JSONObject respAttributes){
+    public SignResponseAttributes(JSONObject respAttributes) throws ParseException {
         filename = respAttributes.getString("filename");
-        if(respAttributes.getString("categories").equals(SignCategories.GLOBAL.toString())){
-            categories = SignCategories.GLOBAL;
-        }
-        else{
-            categories = SignCategories.PSRE;
+        if (respAttributes.getString("categories").equals(AttributeCategory.GLOBAL.toString())) {
+            category = AttributeCategory.GLOBAL;
+        } else {
+            category = AttributeCategory.PSRE;
         }
         docUrl = respAttributes.getString("doc_url");
-        if(respAttributes.getString("signing_status").equals(SigningStatus.IN_PROGRESS.toString())){
+        if (respAttributes.getString("signing_status").equals(SigningStatus.IN_PROGRESS.toString())) {
             signingStatus = SigningStatus.IN_PROGRESS;
         } else if (respAttributes.getString("signing_status").equals(SigningStatus.COMPLETED.toString())) {
             signingStatus = SigningStatus.COMPLETED;
@@ -55,7 +54,7 @@ public class SignResponseAttributes {
         typeOfMaterai = respAttributes.getString("type_of_materai");
         JSONArray getRespSigners = respAttributes.getJSONArray("signers");
         responseSigner = new ResponseSigner[getRespSigners.length()];
-        for(int i = 0; i < getRespSigners.length(); i++){
+        for (int i = 0; i < getRespSigners.length(); i++) {
             JSONObject sign = getRespSigners.getJSONObject(i);
             ResponseSigner resp = new ResponseSigner(sign);
             responseSigner[i] = resp;
@@ -64,17 +63,6 @@ public class SignResponseAttributes {
         //Mengambil dan mengonversi createdAt dan updateAt dari JSON
         createdAt = Date.from(Instant.parse(respAttributes.getString("created_at")));
         updatedAt = Date.from(Instant.parse(respAttributes.getString("update_at")));
-
-    public SignResponseAttributes(JSONObject fromJson) throws ParseException {
-        filename = fromJson.getString("filename");
-        category = AttributeCategory.parse(fromJson.getString("category"));
-        docUrl = fromJson.getString("doc_url");
-        signingStatus = SigningStatus.parse(fromJson.getString("signing_status"));
-        stampingStatus = fromJson.getString("stamping_status");
-        typeOfMaterai = fromJson.optString("type_of_materai");
-        responseSigner = JSONStream.of(fromJson.getJSONArray("signers"), JSONArray::getJSONObject)
-                .map(Try.onFunction(ResponseSigner::new))
-                .toArray(ResponseSigner[]::new);
     }
 
     public String getFilename() {
@@ -114,17 +102,17 @@ public class SignResponseAttributes {
     }
 
     //Metode untuk mengonversi Date ke format ISO 8601
-    public static String convertDateToISO(Date date){
+    public static String convertDateToISO(Date date) {
         return DateTimeFormatter.ISO_INSTANT.format(date.toInstant());
     }
 
     //Metode untuk mendapatkan createdAt dalam format ISO 8601
-    public  String getCreatedAtISO(){
-        return convertDateToISO((createdAt));
+    public String getCreatedAtISO() {
+        return convertDateToISO(createdAt);
     }
 
     //Metode untuk mendapatkan updatedAt dalam format ISO 8601
-    public String getUpdatedISO(){
+    public String getUpdatedISO() {
         return convertDateToISO(updatedAt);
     }
 }
